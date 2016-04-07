2e9ace0800fa691d15c6d6cf6a9faf29805f1afd
Anna Henningsen <anna@addaleax.net>
2016-04-07 02:49:45 +0200

    Add regression tests for npm using different shebangs
    This also belongs to #190, but the new tests include actual
    fake “npm” executables with both absolute shebangs and the
    twisted shebang that comes with current Node.js binary distribution
    tarballs.

--- test/src/nyc-bin.js
+++ test/src/nyc-bin.js
@@ -1,8 +1,11 @@
 /* global describe, it */
 
 var path = require('path')
+var fs = require('fs')
 var spawn = require('child_process').spawn
+var isWindows = require('is-windows')()
 var fixturesCLI = path.resolve(__dirname, '../fixtures/cli')
+var fakebin = path.resolve(fixturesCLI, 'fakebin')
 var bin = path.resolve(__dirname, '../../bin/nyc')
 
 require('chai').should()
@@ -85,6 +88,55 @@ describe('the nyc cli', function () {
       var proc = spawn(process.execPath, args, {
         cwd: directory,
         env: env
+      })
+
+      proc.on('close', function (code) {
+        code.should.equal(0)
+        done()
+      })
+    })
+
+    function writeFakeNPM (shebang) {
+      var targetPath = path.resolve(fakebin, 'npm')
+      var source = fs.readFileSync(path.resolve(fakebin, 'npm-template.js'))
+      fs.writeFileSync(targetPath, '#!' + shebang + '\n' + source)
+      fs.chmodSync(targetPath, 493) // 0o755
+    }
+
+    it('can run "npm test", absolute shebang edition', function (done) {
+      if (isWindows) return done()
+
+      writeFakeNPM(process.execPath)
+
+      var args = [bin, 'npm', 'test']
+      var directory = path.resolve(fixturesCLI, 'run-npm-test-recursive')
+      var proc = spawn(process.execPath, args, {
+        cwd: directory,
+        env: {
+          PATH: fakebin + ':' + env.PATH
+        }
+      })
+
+      proc.on('close', function (code) {
+        code.should.equal(0)
+        done()
+      })
+    })
+
+    it('can run "npm test", weird bash+dirname shebang edition', function (done) {
+      if (isWindows) return done()
+
+      // This string is taken verbatim from tools/install.py in Node core v5.x
+      writeFakeNPM('/bin/sh\n// 2>/dev/null; exec "`dirname "$0"`/node" "$0" "$@"')
+      fs.symlinkSync(process.execPath, path.resolve(fakebin, 'node'))
+
+      var args = [bin, 'npm', 'test']
+      var directory = path.resolve(fixturesCLI, 'run-npm-test-recursive')
+      var proc = spawn(process.execPath, args, {
+        cwd: directory,
+        env: {
+          PATH: fakebin + ':' + env.PATH
+        }
       })
 
       proc.on('close', function (code) {
