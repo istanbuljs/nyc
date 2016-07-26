66a8118d2c24b69d8e1e042671e45d68acd5742e
Mickael Burguet <mickael.burguet@ssense.com>
2016-07-26 13:40:04 -0400

    Add unit test: addAllFiles should work correctly with non-.js files

--- test/fixtures/transpile-hook.js
+++ test/fixtures/transpile-hook.js
@@ -4,3 +4,8 @@ require.extensions['.js'] = function (module, filename) {
   var content = fs.readFileSync(filename, 'utf8');
   module._compile(content.replace('--> pork chop sandwiches <--', ''), filename);
 }
+
+require.extensions['.whatever'] = function (module, filename) {
+  var content = fs.readFileSync(filename, 'utf8');
+  module._compile(content.replace('--> pork chop sandwiches <--', ''), filename);
+}
