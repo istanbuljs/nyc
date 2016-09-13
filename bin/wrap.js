99dbbb3a36c12770686f59fa3ff26fbbdc991e8d
Benjamin E. Coe <bencoe@gmail.com>
2016-09-13 09:52:40 -0700

    feat: refactored config to fix precedence of config vs. args (#388)
    * feat: refactored config to fix precedence of config vs. args see #379
    
    * fix: address standard nits
    
    * fix: remove cruft from package.json
    
    * fix: address @mourner's code review

--- bin/wrap.js
+++ bin/wrap.js
@@ -9,20 +9,14 @@ try {
 var parentPid = process.env.NYC_PARENT_PID || '0'
 process.env.NYC_PARENT_PID = process.pid
 
-;(new NYC({
-  require: process.env.NYC_REQUIRE ? process.env.NYC_REQUIRE.split(',') : [],
-  extension: process.env.NYC_EXTENSION ? process.env.NYC_EXTENSION.split(',') : [],
-  exclude: process.env.NYC_EXCLUDE ? process.env.NYC_EXCLUDE.split(':') : [],
-  include: process.env.NYC_INCLUDE ? process.env.NYC_INCLUDE.split(':') : [],
-  enableCache: process.env.NYC_CACHE === 'enable',
-  sourceMap: process.env.NYC_SOURCE_MAP === 'enable',
-  instrumenter: process.env.NYC_INSTRUMENTER,
-  hookRunInContext: process.env.NYC_HOOK_RUN_IN_CONTEXT === 'enable',
-  showProcessTree: process.env.NYC_SHOW_PROCESS_TREE === 'enable',
-  _processInfo: {
-    ppid: parentPid,
-    root: process.env.NYC_ROOT_ID
-  }
-})).wrap()
+var config = {}
+if (process.env.NYC_CONFIG) config = JSON.parse(process.env.NYC_CONFIG)
+config.enableCache = process.env.NYC_CACHE === 'enable'
+config._processInfo = {
+  ppid: parentPid,
+  root: process.env.NYC_ROOT_ID
+}
+
+;(new NYC(config)).wrap()
 
 sw.runMain()
