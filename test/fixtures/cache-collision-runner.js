5d5041571bccc8fcd0f69100d1b69a0b2e01c90e
isaacs <i@izs.me>
2018-05-08 13:39:20 -0700

    Fix test for node v10 assert.ifError breakage

--- test/fixtures/cache-collision-runner.js
+++ test/fixtures/cache-collision-runner.js
@@ -11,8 +11,8 @@ var workerPath = path.join(__dirname, './cache-collision-worker.js')
 
 function doFork (message) {
   spawn(process.execPath, [workerPath, String(time[0]), String(time[1]), message])
-    .on('close', function (err) {
-      assert.ifError(err)
+    .on('close', function (code) {
+      assert.equal(code, 0, 'received non-zero exit code ' + code)
     })
 }
 
