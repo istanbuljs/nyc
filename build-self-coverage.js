4111a830512619f7a0de9c8e942fd487d99525fa
Corey Farrell <git@cfware.com>
2019-02-22 15:01:48 -0500

    chore: Revert async/await for node.js 6 compatibility.
    This is meant to be kept separate so it can be reverted once we drop
    node.js 6.

--- build-self-coverage.js
+++ build-self-coverage.js
@@ -1,46 +1,42 @@
 const path = require('path')
-const { promisify } = require('util')
 const fs = require('fs')
 const istanbul = require('istanbul-lib-instrument')
 const makeDir = require('make-dir')
-const glob = promisify(require('glob'))
-
-const writeFile = promisify(fs.writeFile)
-const readFile = promisify(fs.readFile)
+const glob = require('glob')
 
 const instrumenter = istanbul.createInstrumenter({
   coverageVariable: '___NYC_SELF_COVERAGE___',
   esModules: true
 })
 
-async function instrumentFile (name) {
+function instrumentFile (name) {
   const indexPath = path.join(__dirname, name)
   const outputPath = path.join(__dirname, 'self-coverage', name)
-  const outputDir = makeDir(path.dirname(outputPath))
 
-  const source = await readFile(indexPath, 'utf8')
+  const source = fs.readFileSync(indexPath, 'utf8')
   const instrumentedSource = name === 'package.json' ? source : instrumenter.instrumentSync(source, indexPath)
 
-  await outputDir
-  await writeFile(outputPath, instrumentedSource)
+  makeDir.sync(path.dirname(outputPath))
+  fs.writeFileSync(outputPath, instrumentedSource)
 }
 
-async function instrumentGlob (pattern) {
-  const result = await glob(pattern, {
+function instrumentGlob (pattern) {
+  const result = glob.sync(pattern, {
     cwd: __dirname,
     nodir: true
   })
 
-  await Promise.all(result.map(instrumentFile))
+  result.forEach(file => {
+    instrumentFile(file)
+  })
 }
 
 function instrumentAll () {
   const globPatterns = ['package.json', 'index.js', 'bin/*.js', 'lib/**/*.js']
 
-  return Promise.all(globPatterns.map(instrumentGlob))
+  globPatterns.forEach(pattern => {
+    instrumentGlob(pattern)
+  })
 }
 
-instrumentAll().catch(error => {
-  console.log(error)
-  process.exit(1)
-})
+instrumentAll()
