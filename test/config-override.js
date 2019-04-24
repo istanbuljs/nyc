1e39ae1aeaff332119a63f380ea2b0a78d6f320a
Corey Farrell <git@cfware.com>
2019-04-24 15:14:01 -0400

    chore: Refresh snapshots, update test/config-override.js to use helpers (#1085)

--- test/config-override.js
+++ test/config-override.js
@@ -1,33 +1,15 @@
-const { spawn } = require('child_process')
-const { resolve } = require('path')
+'use strict'
+
 const t = require('tap')
-const node = process.execPath
-const fixturesCLI = resolve(__dirname, './fixtures/cli')
-const bin = resolve(__dirname, '../self-coverage/bin/nyc')
-const rimraf = require('rimraf').sync
-const tmp = 'conf-override-test'
 
-rimraf(resolve(fixturesCLI, tmp))
-t.teardown(() => rimraf(resolve(fixturesCLI, tmp)))
+const { tempDirSetup, testSuccess } = require('./helpers')
 
-t.test('spawn that does config overriding', t => {
-  const args = [
-    bin, '-t', tmp,
+tempDirSetup(t, __filename)
+
+t.test('spawn that does config overriding', t => testSuccess(t, {
+  args: [
     '--exclude-after-remap=false',
     '--include=conf-override-root.js',
-    node, 'conf-override-root.js'
+    process.execPath, 'conf-override-root.js'
   ]
-  const proc = spawn(node, args, {
-    cwd: fixturesCLI
-  })
-  const out = []
-  const err = []
-  proc.stdout.on('data', c => out.push(c))
-  proc.stderr.on('data', c => err.push(c))
-  proc.on('close', (code, signal) => {
-    t.equal(code, 0)
-    t.equal(signal, null)
-    t.matchSnapshot(Buffer.concat(out).toString(), 'stdout')
-    t.end()
-  })
-})
+}))
