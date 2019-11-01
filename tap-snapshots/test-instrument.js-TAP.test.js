65c6529a3ed80eb0809e4ddf2433972c61efec41
Corey Farrell <git@cfware.com>
2019-11-01 11:48:53 -0400

    chore: Add failing test for --all on pre-instrumented code

---
+++ tap-snapshots/test-instrument.js-TAP.test.js
@@ -0,0 +1,17 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/instrument.js TAP can write files in place with --in-place switch > stdout 1`] = `
+----------|---------|----------|---------|---------|-------------------
+File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s 
+----------|---------|----------|---------|---------|-------------------
+All files |       0 |      100 |       0 |       0 |                   
+ file1.js |       0 |      100 |       0 |       0 | 2,5               
+ file2.js |       0 |      100 |       0 |       0 | 2,5               
+----------|---------|----------|---------|---------|-------------------
+
+`
