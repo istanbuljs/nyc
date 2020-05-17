bc545b7c200d540f0db337942bdf84684aa57e15
Brett Zamir <brettz9@yahoo.com>
2020-05-17 15:07:48 +0800

    feat: support passing `--reporter-options` to nyc's own reporters

--- index.js
+++ index.js
@@ -446,6 +446,7 @@ class NYC {
       reports.create(_reporter, {
         skipEmpty: this.config.skipEmpty,
         skipFull: this.config.skipFull,
+        reporterOptions: this.config.reporterOptions,
         projectRoot: this.cwd,
         maxCols: process.stdout.columns || 100
       }).execute(context)
