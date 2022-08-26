4e79f68a8e305b357497d57e731d23b219239927
cschanaj <cschanaj@users.noreply.github.com>
2022-08-26 21:54:53 +0800

    chore(deps-dev): bump deprecate package uuid@3.x

--- self-coverage-helper.js
+++ self-coverage-helper.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const fs = require('fs')
-const uuid = require('uuid/v4')
+const { v4: uuidv4 } = require('uuid')
 const mkdirp = require('make-dir')
 const onExit = require('signal-exit')
 const nodePreload = require('node-preload')
@@ -21,7 +21,7 @@ global[nycSelfCoverageHelper] = {
     const selfCoverageDir = path.join(__dirname, '.self_coverage')
     mkdirp.sync(selfCoverageDir)
     fs.writeFileSync(
-      path.join(selfCoverageDir, uuid() + '.json'),
+      path.join(selfCoverageDir, uuidv4() + '.json'),
       JSON.stringify(coverage),
       'utf-8'
     )
