b532e5c9bc59eca09255a7cfcbd4ea7eeb4651f9
cschanaj <cschanaj@users.noreply.github.com>
2022-12-04 12:31:19 +0800

    revert: "fix: breakage due to glob version changes"
    This reverts commit 341deeb901140656876c7f8709c2fbe6db62673b.

--- build-self-coverage.js
+++ build-self-coverage.js
@@ -24,7 +24,7 @@ function instrumentFile (name) {
 
 function instrumentGlob (pattern) {
   const result = glob.sync(pattern, {
-    cwd: path.normalize(__dirname),
+    cwd: __dirname,
     nodir: true
   })
 
